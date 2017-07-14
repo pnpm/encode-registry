ffb9ea4f3743af25cc6e6d03599e0b629e78ee86
zkochan <zoltan.kochan@gmail.com>
2017-07-14 23:39:10 +0300

    feat: init

---
+++ test.js
@@ -0,0 +1,14 @@
+'use strict'
+const encodeRegistry = require('.')
+const test = require('tape')
+
+test('encodeRegistry()', t => {
+  t.equal(encodeRegistry('https://registry.npmjs.org/'), 'registry.npmjs.org')
+  t.equal(encodeRegistry('https://localhost:4873/'), 'localhost+4873')
+  t.equal(encodeRegistry('https://sinopia/'), 'sinopia')
+
+  t.throws(() => encodeRegistry(), '`registry` is required')
+  t.throws(() => encodeRegistry(1), '`registry` should be a string')
+
+  t.end()
+})
